d65aec69ca6882db451c127fe07c4e278d66ed9f
foresightyj <imyuanjian@gmail.com>
2019-08-03 15:15:29 +0800

    stuck with sqlite3 again

--- src/repositories/dbConnection.ts
+++ src/repositories/dbConnection.ts
@@ -4,13 +4,9 @@ import "reflect-metadata";
 import { User } from "../entity/User";
 import { Schedule } from "../entity/Schedule";
 
-
 export default createConnection({
-    type: "mssql",
-    host: "192.168.0.112",
-    database: "db009",
-    username: "fht360",
-    password: "fht#^)2014",
+    type: "sqlite",
+    database: "schedules.db",
     synchronize: true,
     logging: true,
     entities: [User, Schedule],
