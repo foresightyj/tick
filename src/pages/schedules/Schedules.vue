6e992b21777c55b8c67614b6d0320c977df1bdce
foresightyj <imyuanjian@gmail.com>
2019-08-06 16:22:41 +0800

    more update

--- src/pages/schedules/Schedules.vue
+++ src/pages/schedules/Schedules.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
     <el-input v-model="schedule_filter" placeholder="Search..." autofocus></el-input>
-    <el-table :data="schedules" style="width: 100%" :row-class-name="tableRowClassName">
-      <el-table-column label="期限" width="180">
-        <template slot-scope="scope">
-          {{scope.row.due | formatDate}}
-          <!-- <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>-->
-        </template>
-      </el-table-column>
-      <el-table-column prop="task" label="任务" width="180"></el-table-column>
-    </el-table>
+    <div v-for="section in sections" :key="section.title">
+      <div v-if="section.schedules.length">
+        <h3>{{section.title}}</h3>
+        <el-table :data="section.schedules" style="width: 100%" :row-class-name="tableRowClassName">
+          <el-table-column label="期限" width="240">
+            <template slot-scope="scope">
+              <el-date-picker
+                :value="scope.row.due"
+                @input="onPickerInput(scope.row, $event)"
+                type="datetime"
+                placeholder="选择日期时间"
+                align="right"
+                :picker-options="pickerOptions"
+              ></el-date-picker>
+            </template>
+          </el-table-column>
+          <el-table-column prop="task" label="任务" width="360"></el-table-column>
+        </el-table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Button, Table, TableColumn, Input } from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import { Scheduler } from "../../scheduler/Scheduler";
 import { Schedule } from "../../entity/Schedule";
@@ -25,7 +34,14 @@ const scheduler = remote.getGlobal("scheduler") as Scheduler;
 
 import moment from "moment";
 
+import { Button, Table, TableColumn, Input, DatePicker } from "element-ui";
 import { rowCallbackParams } from "element-ui/types/table";
+
+interface Section {
+  title: string,
+  background: string,
+  schedules: Schedule[]
+}
 
 export default Vue.extend({
   name: "app",
@@ -33,7 +49,8 @@ export default Vue.extend({
     [Button.name]: Button,
     [Input.name]: Input,
     [Table.name]: Table,
-    [TableColumn.name]: TableColumn
+    [TableColumn.name]: TableColumn,
+    [DatePicker.name]: DatePicker,
   },
   filters: {
     formatDate(d: Date) {
@@ -43,21 +60,43 @@ export default Vue.extend({
   data() {
     return {
       schedule_filter: "",
-      schedules: [] as Schedule[]
+      schedules: [] as Schedule[],
+      pickerOptions: {
+        shortcuts: [{
+          text: '今天',
+          onClick(picker: DatePicker) {
+            picker.$emit('pick', new Date());
+          }
+        }, {
+          text: '昨天',
+          onClick(picker: DatePicker) {
+            const date = new Date();
+            date.setTime(date.getTime() - 3600 * 1000 * 24);
+            picker.$emit('pick', date);
+          }
+        }, {
+          text: '一周前',
+          onClick(picker: DatePicker) {
+            const date = new Date();
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', date);
+          }
+        }]
+      },
     };
   },
   computed: {
-    sections: function () {
+    sections(): Section[] {
       let startOfToday = moment().startOf('day').toDate()
       let startOf3DaysAgo = moment().subtract(3, 'days').startOf('day').toDate()
       let startOfTomorrow = moment().add(1, 'days').startOf('day').toDate()
       let startOfTheDayAfterTomorrow = moment().add(1, 'days').startOf('day').toDate()
 
-      let schedules_Today = this.schedules.filter(s => s.due >= startOfToday && s.due < startOfTomorrow)
-      let schedules_Tomorrow = this.schedules.filter(s => s.due >= startOfTomorrow && s.due < startOfTheDayAfterTomorrow)
-      let schedules_TheDayAfterTomorrow = this.schedules.filter(s => s.due >= startOfTheDayAfterTomorrow)
-      let schedules_InPast3Days = this.schedules.filter(s => s.due >= startOf3DaysAgo && s.due < startOfToday)
-      let schedules_MoreThan3DaysAgo = this.schedules.filter(s => s.due < startOf3DaysAgo)
+      let schedules_Today = this.schedules.filter(s => s.due >= startOfToday && s.due < startOfTomorrow);
+      let schedules_Tomorrow = this.schedules.filter(s => s.due >= startOfTomorrow && s.due < startOfTheDayAfterTomorrow);
+      let schedules_TheDayAfterTomorrow = this.schedules.filter(s => s.due >= startOfTheDayAfterTomorrow);
+      let schedules_InPast3Days = this.schedules.filter(s => s.due >= startOf3DaysAgo && s.due < startOfToday);
+      let schedules_MoreThan3DaysAgo = this.schedules.filter(s => s.due < startOf3DaysAgo);
 
       if (this.schedule_filter) {
         schedules_Today = schedules_Today.filter(s => decodeURI(s.task).toLowerCase().indexOf(this.schedule_filter.toLowerCase()) > -1)
@@ -92,7 +131,7 @@ export default Vue.extend({
         background: '#f2f2f2',
         schedules: schedules_MoreThan3DaysAgo
       }
-      ]
+      ];
     }
   },
   async mounted() {
@@ -111,12 +150,18 @@ export default Vue.extend({
   },
   methods: {
     tableRowClassName(row: rowCallbackParams) {
+      const section = row.row as Section;
       if (row.rowIndex === 1) {
         return "warning-row";
       } else if (row.rowIndex === 3) {
         return "success-row";
       }
       return "";
+    },
+    onPickerInput(schedule: Schedule, event: Date) {
+      schedule.due = event;
+      this.schedules.splice(this.schedules.findIndex(s => s.id === schedule.id), 1, schedule);
+      scheduler.update_due(schedule);
     }
   }
 });
